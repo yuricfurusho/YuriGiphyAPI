eb28b59ce1a74cdf95be0623bd665c7280501f2c
Yuri Furusho <yuricfurusho@gmail.com>
2018-09-20 00:38:36 -0300

    download Draft

--- app/src/main/java/com/yuricfurusho/yurigiphyapi/MainActivity.kt
+++ app/src/main/java/com/yuricfurusho/yurigiphyapi/MainActivity.kt
@@ -3,18 +3,26 @@ package com.yuricfurusho.yurigiphyapi
 import android.app.SearchManager
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.support.design.widget.TabLayout
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.SearchView
 import android.view.Menu
 import com.bumptech.glide.Glide
-import com.yuricfurusho.yurigiphyapi.R.id.*
+import com.bumptech.glide.request.target.SimpleTarget
+import com.bumptech.glide.request.target.Target
+import com.bumptech.glide.request.target.ViewTarget
 import com.yuricfurusho.yurigiphyapi.adapters.GiphyPagerAdapter
 import com.yuricfurusho.yurigiphyapi.fragments.FavoritesFragment
 import com.yuricfurusho.yurigiphyapi.fragments.OnListFragmentInteractionListener
 import com.yuricfurusho.yurigiphyapi.fragments.TrendingFragment
 import com.yuricfurusho.yurigiphyapi.model.Data
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.ByteArrayOutputStream
+import java.io.File
+
 
 class MainActivity : AppCompatActivity(), OnListFragmentInteractionListener {
     override fun onAddToFavorite(data: Data) {
@@ -26,11 +34,56 @@ class MainActivity : AppCompatActivity(), OnListFragmentInteractionListener {
         trendingFrag.updateFavoriteList(data)
         favoritesFrag.updateFavoriteList(data)
 
-        Glide.with(applicationContext)
+
+
+//
+//        val futureTarget = Glide.with(applicationContext)
+//                .asBitmap()
+//                .load(data.images.fixedHeightDownsampled.url)
+//                .submit()
+//
+//
+//
+//
+//        val bitmap = futureTarget.get()
+
+//
+//        val into = Glide.with(applicationContext)
+//                .load(data.images.fixedHeightDownsampled.url)
+//                .into(Target<Bitmap>());
+
+        val target = Target<Bitmap>()
+        val into = Glide.with(applicationContext)
                 .asBitmap()
                 .load(data.images.fixedHeightDownsampled.url)
-                .in
+                .into(target)
 
+
+        val stream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
+        val byteArray = stream.toByteArray()
+        bitmap.recycle()
+
+        byteArray
+
+        val filename = data.id + ".png"
+        openFileOutput(filename, Context.MODE_PRIVATE).use {
+            it.write(byteArray)
+        }
+
+        // Do something with the Bitmap and then when you're done with it:
+        Glide.with(applicationContext).clear(futureTarget)
+
+
+        val fileStreamPath: File = getFileStreamPath(filename)
+        fileStreamPath.absolutePath
+
+
+
+
+        Glide.with(applicationContext)
+                .load(fileStreamPath.absolutePath)
+                .into(imgDeTESTE)
     }
 
     private var giphyPagerAdapter: GiphyPagerAdapter? = null
