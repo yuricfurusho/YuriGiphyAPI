c6f08e32dadbdb1261623a00c289b54cb23527aa
Yuri Furusho <yuricfurusho@gmail.com>
2018-09-25 23:23:04 -0300

    Dao Room interface

---
+++ app/src/main/java/com/yuricfurusho/yurigiphyapi/model/DataDao.kt
@@ -0,0 +1,21 @@
+package com.yuricfurusho.yurigiphyapi.model
+
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.Query
+
+/**
+ * Created by Yuri Furusho on 25/09/18.
+ */
+@Dao
+interface DataDao {
+
+    @Query("SELECT * from Data ORDER BY id ASC")
+    fun getAllData(): List<Data>
+
+    @Insert
+    fun insert(data: Data)
+
+    @Query("DELETE FROM Data")
+    fun deleteAll()
+}
