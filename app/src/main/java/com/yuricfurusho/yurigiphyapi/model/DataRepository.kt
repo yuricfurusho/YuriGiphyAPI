c4611a4627aa7521a76d2e8f07a94fdee1628383
Yuri Furusho <yuricfurusho@gmail.com>
2018-09-28 00:46:41 -0300

    Repository creation
    This class decides where the data comes from.
    From a remote server or a local database for example.

---
+++ app/src/main/java/com/yuricfurusho/yurigiphyapi/model/DataRepository.kt
@@ -0,0 +1,18 @@
+package com.yuricfurusho.yurigiphyapi.model
+
+import android.arch.lifecycle.LiveData
+import android.support.annotation.WorkerThread
+
+/**
+ * Created by Yuri Furusho on 28/09/18.
+ */
+class DataRepository(private val dao: DataDao) {
+
+    val allData: LiveData<List<Data>> = dao.getAllData()
+
+    @WorkerThread
+    suspend fun insert(data: Data) {
+        dao.insert(data)
+    }
+
+}
